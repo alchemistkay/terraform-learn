fb779c66b5e1f0c46c5a78e83d49c8c7b3887203
cloud-dev101 <72142185+cloud-dev101@users.noreply.github.com>
2024-10-04 20:08:13 +0100

    terraform EC2 provisioning

--- main.tf
+++ main.tf
@@ -1,40 +1,155 @@
-provider "aws" {}
-
-variable subnet_cidr_block {
-  type        = string
-  default     = "10.0.32.0/24"
-  description = "subnet cidr block"
+provider "aws" {
+  region = "eu-west-2"
 }
 
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
 
-resource "aws_vpc" "development-vpc" {
-  cidr_block       = "10.0.0.0/16"
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
 
   tags = {
-    Name = "development-vpc"
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id     = aws_vpc.development-vpc.id
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id     = aws_vpc.myapp-vpc.id
   cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
 
   tags = {
-    Name = "dev-subnet-1"
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
 
-output dev-vpc-id {
-  value       = "aws_vpc.development-vpc.id"
-  sensitive   = true
-  description = "the id of the development vpc created"
-  depends_on  = []
+# The below code creates a route table that allow traffic from all IPs and also add an internet gateway
+# this is another way of allowing all traffic into the VPC and also adding an internet gateway
+
+# resource "aws_route_table" "myapp-route-table" {
+#   vpc_id = aws_vpc.myapp-vpc.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.myapp-igw.id
+#   }
+
+#   tags = {
+#     Name = "${var.env_prefix}-rtb"
+#   }
+
+# }
+
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#   subnet_id = aws_subnet.myapp-subnet-1.id
+#   route_table_id = aws_route_table.myapp-route-table.id
+# }
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name: "${var.env_prefix}-main-rtb"
+  }
 }
 
-output dev-subnet-id {
-  value       = "aws_subnet.dev-subnet-1.id"
-  sensitive   = true
-  description = "the id of the subnet created in the development vpc"
-  depends_on  = []
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name: "${var.env_prefix}-igw"
+  }
 }
+
+# creating security group fot the VPC using the default security group
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-default-sg"
+  }
+}
+
+# creating an EC2 insatance with ssh login
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name" 
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws-ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+
+  user_data = file("entry-script.sh")
+  
+  user_data_replace_on_change = true
+
+  tags = {
+    Name: "${var.env_prefix}-server"
+  }
+}
+
+output "ec2-public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+
+
+
+
 
